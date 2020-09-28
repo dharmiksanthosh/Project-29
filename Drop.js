d6a1a6e7b2a9f318f899182299fcc0161358ad74
Dharmik Santhosh Raghav <70014740+dharmiksanthosh@users.noreply.github.com>
2020-09-28 19:39:11 +0530

    Delete Drop.js

--- Drop.js
+++
@@ -1,9 +0,0 @@
-function isTouching(lstone,num){
-  var distance = dist(lstone.body.position.x,lstone.body.position.y,mangoes[num].body.position.x,mangoes[num].body.position.y)
-	
-	if 	(distance <=lstone.radius+mangoes[num].radius/2)
-		{
-		console.log(true);
-		Matter.Body.setStatic(mangoes[num].body,false);
-  } 
-  }
